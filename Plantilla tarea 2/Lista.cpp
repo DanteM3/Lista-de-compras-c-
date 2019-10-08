626c33c543d81d2f5e37e3dad9a46da4e90e9e9a
DanteM3 <56320125+DanteM3@users.noreply.github.com>
2019-10-08 17:08:53 -0400

    Add files via upload

---
+++ Plantilla tarea 2/Lista.cpp
@@ -0,0 +1,57 @@
+#include "Lista.h"
+
+Lista::Lista(): _primer(NULL) , _ultimo(NULL)
+{
+
+}
+
+void Lista::Agregar(Elemento* elemento)
+{
+	/*IMPLEMENTE ESTE METODO
+		Dado un objeto de tipo puntero a Elemento agréguelo a la lista.
+
+		Tenga pendiente que el atributo _primer guarda un puntero al primer elemento de la lista y que _ultimo es un puntero al al último elemento de la lista
+	*/
+	if(_primer ==NULL){
+			_primer = elemento;
+			_ultimo = elemento;
+
+		}
+	else{
+		_ultimo= elemento;
+	}
+
+
+}
+
+bool Lista::Eliminar(int indice)
+{
+if(indice>0){
+
+
+			return true;
+		}
+	   return false;
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	IMPLEMENTE ESTE METODO
+	Dado un indice entero elimine el objeto en esa posición.  Ejemplo si el parámetro indice es igual a 0 se eliminará el primer elemento.
+
+	Esta función retorna un booleano,  retorno verdadero si el índice está contenido en la lista.
+
+	Ejemplo, si la lista tiene 4 elementos y se pide eliminar el índice 9 este método retornará falso porque ese índice no está contenido en la lista.
+	*/
+
+
+}
+
